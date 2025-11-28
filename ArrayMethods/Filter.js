a6688407b205b2b8977e698b558fbe3ed9318859
Markjonel02 <rellesmarkjonel14@gmail.com>
2025-11-28 10:08:30 +0800

    trying new filtermethod

--- ArrayMethods/Filter.js
+++ ArrayMethods/Filter.js
@@ -9,3 +9,18 @@ const Filteruser = (name, isactive) => {
 };
 
 console.log(Filteruser());
+
+const users = [
+  { id: 1, name: "Alice", status: "active" },
+  { id: 2, name: "Bob", status: "pending" },
+  { id: 3, name: "Charlie", status: "active" },
+  { id: 4, name: "Diana", status: "pending" },
+  { id: 5, name: "Eve", status: "pending" },
+];
+
+// TODO: Filter only active users
+const activeUsers = users
+  .filter((u) => u.status === "active")
+  .map((user) => user.name);
+
+console.log(activeUsers); // Output: ['Alice', 'Charlie', 'Eve']5
